2e71db50869b37ede4c26587c8bbc40152164b2e
PhamXuanSang <101335730+phamxuansanga11@users.noreply.github.com>
2022-07-15 16:34:15 +0700

    update validate field fullName type value

--- src/components/contactForm/ContactForm.js
+++ src/components/contactForm/ContactForm.js
@@ -20,7 +20,6 @@ function ContactForm({ myRef }) {
     register,
     handleSubmit,
     watch,
-    getValues,
     formState: { errors },
   } = useForm();
 
@@ -32,7 +31,7 @@ function ContactForm({ myRef }) {
     EMAIL: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/i,
     PHONE: /(84|0[3|5|7|8|9])+([0-9]{8})\b/i,
     FB: /(?:https?:\/\/)?(?:www\.)?facebook\.com\/(?:(?:\w\.)*#!\/)?(?:pages\/)?(?:[\w\-\.]*\/)*([\w\-\.]*)/i,
-    VARIABLE_NAME: /^[a-zA-Z_$][a-zA-Z_$0-9]*$/i,
+    VARIABLE_NAME: /^[a-zA-Z-,]+(\s{0,1}[a-zA-Z-, ])*$/,
     ONLY_TWO_NUMBER: /^[0-9\b]{0,2}$/,
     ONLY_NUMBER: /^[0-9]*$/,
     LINK: /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g,
@@ -75,13 +74,13 @@ function ContactForm({ myRef }) {
                         value: 4,
                         message: "Họ tên phải tối thiểu 4 kí tự.",
                       },
-                      pattern: {
-                        value: /d+/,
-                        message: "Vui lòng nhập chữ.",
-                      },
                       maxLength: {
                         value: 14,
                         message: "Hộ tên tối đa 14 kí tự.",
+                      },
+                      pattern: {
+                        value: PATTERN.VARIABLE_NAME,
+                        message: "Vui lòng nhập chữ.",
                       },
                     })}
                     style={handleStyle("fullName")}
